2a60d1821836c78b26a7d496bb8b7045a5d47970
VadymQA <58395732+VadymQA@users.noreply.github.com>
2023-04-25 21:10:54 +0300

    Add files via upload
    Lab 1

---
+++ lab1/check_response.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+URL="https://www.google.com/"
+LOG_FILE="response_log.txt"
+EMAIL="Vadym.Hnatenko@infiz.khpi.edu.ua"
+
+echo "Running the script..."
+
+response_code=$(curl -o /dev/null -s -w "%{http_code}" $URL)
+
+echo "Response code: $response_code"
+if [[ $response_code != 4* ]] && [[ $response_code != 3* ]]; then
+	echo "$(date) - Response code: $response_code" >> $LOG_FILE
+	echo -e "Subject: Wrong web server response! Response code: $response_code" | msmtp $EMAIL
+	echo "Warning! Response code not 2xx or 3xx!"
+fi
+
+echo "Script execution complete."
