c1ba6bac079130ebb9bcef38a69aca3f376da74f
Vadim <vadonvadim@gmail.com>
2023-05-20 19:00:01 +0300

    lab3

--- lab1/check_response.sh
+++ lab1/check_response.sh
@@ -8,9 +8,8 @@ echo "Running the script..."
 response_code=$(curl -o /dev/null -s -w "%{http_code}" $URL)
 
 echo "Response code: $response_code"
-if [[ $response_code != 4* ]] && [[ $response_code != 3* ]]; then
+if [[ $response_code != 2* ]] && [[ $response_code != 3* ]]; then
 	echo "$(date) - Response code: $response_code" >> $LOG_FILE
-	echo -e "Subject: Wrong web server response! Response code: $response_code" | msmtp $EMAIL
 	echo "Warning! Response code not 2xx or 3xx!"
 fi
 
